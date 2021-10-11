6bcf0af20bb06654b6060c847c1078ae26f85c18
Yuchao Dai <icyblade.py@gmail.com>
2021-10-12 01:55:50 +0800

    Use effective health as mitigation point

--- Rawr.Tankadin/CalculationsTankadin.cs
+++ Rawr.Tankadin/CalculationsTankadin.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -44,24 +44,18 @@ namespace Rawr.Tankadin
 					"Complex Stats:Mitigation",
 					"Complex Stats:Total Mitigation",
 					"Complex Stats:Chance to be Crit",
-					@"Complex Stats:Overall Points*Overall Points are a sum of Mitigation and Survival Points. 
-Overall is typically, but not always, the best way to rate gear. 
-For specific encounters, closer attention to Mitigation and 
+					@"Complex Stats:Overall Points*Overall Points are a sum of Mitigation and Survival Points.
+Overall is typically, but not always, the best way to rate gear.
+For specific encounters, closer attention to Mitigation and
 Survival Points individually may be important.",
-					@"Complex Stats:Mitigation Points*Mitigation Points represent the amount of damage you mitigate, 
-on average, through armor mitigation and avoidance. It is directly 
-relational to your Damage Taken. Ideally, you want to maximize 
-Mitigation Points, while maintaining 'enough' Survival Points 
-(see Survival Points). If you find yourself dying due to healers 
-running OOM, or being too busy healing you and letting other 
-raid members die, then focus on Mitigation Points.",
-					@"Complex Stats:Survival Points*Survival Points represents the total raw physical damage 
-(pre-mitigation) you can take before dying. Unlike 
-Mitigation Points, you should not attempt to maximize this, 
-but rather get 'enough' of it, and then focus on Mitigation. 
-'Enough' can vary greatly by fight and by your healers, but 
-keeping it roughly even with Mitigation Points is a good 
-way to maintain 'enough' as you progress. If you find that 
+					@"Complex Stats:Mitigation Points*Effective health",
+					@"Complex Stats:Survival Points*Survival Points represents the total raw physical damage
+(pre-mitigation) you can take before dying. Unlike
+Mitigation Points, you should not attempt to maximize this,
+but rather get 'enough' of it, and then focus on Mitigation.
+'Enough' can vary greatly by fight and by your healers, but
+keeping it roughly even with Mitigation Points is a good
+way to maintain 'enough' as you progress. If you find that
 you are being killed by burst damage, focus on Survival Points.",
                      "Threat:Overall",
                      "Threat:Holy Shield",
@@ -173,7 +167,7 @@ you are being killed by burst damage, focus on Survival Points.",
             float attacks = calcOpts.NumberAttackers / calcOpts.AttackSpeed * 10;
             //Apply armor and multipliers for each attack type...
             float miss = Math.Min(0.01f * attacks * calculatedStats.Avoidance, attacks);
-            
+
             float block = Math.Min(Math.Min(8, attacks * (.3f + character?.Ranged?.Id == 29388 ? 42f / 7.884614944458f : 0 + 0.01f*calculatedStats.Block)), attacks - miss);
             if (block > 8) block += Math.Min((attacks - block) * .01f * calculatedStats.Block, attacks - miss - block);
             float crit = Math.Min(0.01f * Math.Max(5 - calculatedStats.CritAvoidance, 0) * attacks, attacks - miss - block);
@@ -191,13 +185,13 @@ you are being killed by burst damage, focus on Survival Points.",
             calculatedStats.TotalMitigation = 100f - calculatedStats.DamageTaken;
 
             calculatedStats.SurvivalPoints = stats.Health / reduction;
-            calculatedStats.MitigationPoints = calcOpts.MitigationScale * (1f * (1f / (calculatedStats.DamageTaken / 100f)));
+            calculatedStats.MitigationPoints = stats.Health / calculatedStats.DamageTaken * 100;
             float ws = character.MainHand == null ? 0 : character.MainHand.Speed;
             float wd = character.MainHand == null ? 0 : ((character.MainHand.MinDamage + character.MainHand.MaxDamage) / 2f);
             calculatedStats.SoRTPS = ws == 0 ? 0 : ((0.85f * (2610.43f * ws / 100f) + 0.03f * wd - 1f + (0.108f * ws * stats.SpellDamageRating * ws)) / ws) * modifier;
             calculatedStats.ConsecrateTPS = calcOpts.NumberAttackers * (512 + .9524f * stats.SpellDamageRating) / 8f * modifier;
             calculatedStats.JoRTPS = (218f + stats.SpellDamageRating * .7143f) / 9f * modifier;
-            calculatedStats.OverallTPS = calculatedStats.SoRTPS + calculatedStats.JoRTPS + 
+            calculatedStats.OverallTPS = calculatedStats.SoRTPS + calculatedStats.JoRTPS +
                 calculatedStats.HolyShieldTPS + calculatedStats.ConsecrateTPS + calculatedStats.MiscTPS;
             calculatedStats.ThreatPoints = calculatedStats.OverallTPS * calcOpts.ThreatScale;
 
